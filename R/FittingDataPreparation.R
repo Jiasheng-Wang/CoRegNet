2038a8679bbb4a526875423e64af1d3b7ddefe98
Jiasheng <53879608+W765623192@users.noreply.github.com>
2023-01-16 13:03:23 -0600

    Update

--- R/FittingDataPreparation.R
+++ R/FittingDataPreparation.R
@@ -17,103 +17,120 @@
 #' @examples
 #' FittingFile_Dir = "~/CopNet_Test/FittingData/", Transform_CoPM_Dir = "~/CopNet_Test/TransformedMatrix/", SigIdx = fitting_summary$qualified_gene_idx, P_cutoff = P_cutoff, FittingFiles = FittingFiles)
 
-FittingDataPreparation <- function(FittingFile_Dir,Transform_CoPM_Dir,CoPMatrix,SigIdx,P_cutoff,FittingFiles){
-  if(dir.exists(FittingFile_Dir)){
-  }else{
+FittingDataPreparation <- function(FittingFile_Dir,Transform_CoPM_Dir,CoPMatrix,SigIdx,P_cutoff,FittingFiles,trans_summary,Ncores){
+  if (dir.exists(FittingFile_Dir)) {
+  }
+  else {
     stop("Error, save_path not found")
   }
-
-  ##Real Co-perturbation Times:
-  RealCoTimes <- CoPMatrix[upper.tri(CoPMatrix,diag = FALSE)]
-  RealCoTimes <- RealCoTimes[SigIdx]
-
-  ##Max Co-perturbation Times:
-  ColsumVector <- diag(CoPMatrix)
-  Max_CoPTimes <-  which(upper.tri(CoPMatrix,diag = FALSE), arr.in=TRUE)
-  Max_CoPTimes_sig <- Max_CoPTimes[SigIdx,]
-  Max_CoPTimes <- cbind.data.frame(GenePair_Idx = SigIdx,
-                                   N1 = ColsumVector[Max_CoPTimes_sig[,1]],
-                                   N2 = ColsumVector[Max_CoPTimes_sig[,2]])
-  Max_CoPTimes$diff <-  Max_CoPTimes$N1-Max_CoPTimes$N2
-  Max_CoPTimes$MaxCoperturbationTimes <- ifelse(test = Max_CoPTimes$diff >0, yes = Max_CoPTimes$N2, no = Max_CoPTimes$N1)
-  Max_CoperturbationTimes <- Max_CoPTimes[,c("GenePair_Idx","MaxCoperturbationTimes")]
-
-  ##Combined the parameters:
-  FITTINGPARAMETERS <- cbind.data.frame(Max_CoperturbationTimes,CoperturbationTimes = RealCoTimes)
-  rm(Max_CoperturbationTimes,Max_CoPTimes,Max_CoPTimes_sig,RealCoTimes)
-  ##SplitFiles:
-  SplitContinuousNumber <- function(Number, nsplits){
-    n <- Number
-    l <- n %/% nsplits
-    k <- n %% nsplits
-
-    startend <- data.frame(matrix(nrow = nsplits, ncol = 3,0), stringsAsFactors = FALSE)
-    length_seq <- rep(l,nsplits)
-    if (k == 0) {
-      #No need to add anything
-    }else{
-      length_seq[1:k] <- length_seq[1:k]+1
+  RealCoTimes <- CoPMatrix[upper.tri(CoPMatrix, diag = FALSE)]
+  Nfiles <- nrow(trans_summary)
+  Assignment_table <- data.frame(matrix(nrow = Nfiles,
+                                        ncol = 4))
+  colnames(Assignment_table) <- c("idx", "GenePairs", "istart",
+                                  "iend")
+  Assignment_table$idx <- seq(1, Nfiles)
+  Assignment_table$GenePairs <- trans_summary$`#GenePairs`
+  Assignment_table$istart[1] <- 1
+  Assignment_table$iend[nrow(Assignment_table)] <- sum(Assignment_table$GenePairs)
+  if (nrow(Assignment_table) >= 2) {
+    for (ii in 1:(nrow(Assignment_table) - 1)) {
+      Assignment_table$iend[ii] <- Assignment_table$istart[ii] +
+        Assignment_table$GenePairs[ii] - 1
+      Assignment_table$istart[ii + 1] <- Assignment_table$iend[ii] +
+        1
     }
-
-
-    colnames(startend) <- c("Start","End","Length")
+  }else {
+  }
+  RealCoTimes <- RealCoTimes[SigIdx]
+  ColsumVector <- diag(CoPMatrix)
+  Max_CoPTimes <- which(upper.tri(CoPMatrix, diag = FALSE),
+                        arr.in = TRUE)
+  Max_CoPTimes_sig <- Max_CoPTimes[SigIdx, ]
+  Max_CoPTimes <- cbind.data.frame(GenePair_Idx = SigIdx,
+                                   N1 = ColsumVector[Max_CoPTimes_sig[, 1]], N2 = ColsumVector[Max_CoPTimes_sig[,
+                                                                                                                2]])
+  Max_CoPTimes$diff <- Max_CoPTimes$N1 - Max_CoPTimes$N2
+  Max_CoPTimes$MaxCoperturbationTimes <- ifelse(test = Max_CoPTimes$diff >
+                                                  0, yes = Max_CoPTimes$N2, no = Max_CoPTimes$N1)
+  Max_CoperturbationTimes <- Max_CoPTimes[, c("GenePair_Idx",
+                                              "MaxCoperturbationTimes")]
+  FITTINGPARAMETERS <- cbind.data.frame(Max_CoperturbationTimes,
+                                        CoperturbationTimes = RealCoTimes)
+  rm(Max_CoperturbationTimes, Max_CoPTimes, Max_CoPTimes_sig,
+     RealCoTimes)
+  gc()
+  SplitContinuousNumber <- function(Number, nsplits) {
+    n <- Number
+    l <- n%/%nsplits
+    k <- n%%nsplits
+    startend <- data.frame(matrix(nrow = nsplits, ncol = 3,
+                                  0), stringsAsFactors = FALSE)
+    length_seq <- rep(l, nsplits)
+    if (k == 0) {
+    }
+    else {
+      length_seq[1:k] <- length_seq[1:k] + 1
+    }
+    colnames(startend) <- c("Start", "End", "Length")
     startend$Length <- length_seq
     startend$Start[1] <- 1
-    startend$End[1] <- startend$Start[1] + startend$Length[1] - 1
+    startend$End[1] <- startend$Start[1] + startend$Length[1] -
+      1
     if (nrow(startend) > 1) {
       for (ii in 2:nrow(startend)) {
-        startend$Start[ii] <- startend$End[ii-1] + 1
-        startend$End[ii] <- startend$Start[ii] + startend$Length[ii] - 1
+        startend$Start[ii] <- startend$End[ii - 1] +
+          1
+        startend$End[ii] <- startend$Start[ii] + startend$Length[ii] -
+          1
       }
-    }else{
-      #Done
+    }
+    else {
     }
     return(startend)
   }
-  SplitMatrix <- SplitContinuousNumber(length(SigIdx),FittingFiles)
+  SplitMatrix <- SplitContinuousNumber(length(SigIdx), FittingFiles)
+  SigIdx_list <- vector("list",length = FittingFiles)
   for (i in 1:nrow(SplitMatrix)) {
-    FittingParameters <- FITTINGPARAMETERS[SplitMatrix$Start[i]:SplitMatrix$End[i],]
-    ##load the data of real co-perturbation times & max co-perturbation times:
-    save(FittingParameters, file = paste0(FittingFile_Dir,"/FittingParameters_",i,".RData"))
+    FittingParameters <- FITTINGPARAMETERS[SplitMatrix$Start[i]:SplitMatrix$End[i], ]
+    SigIdx_list[[i]] <- FittingParameters$GenePair_Idx
+    save(FittingParameters, file = paste0(FittingFile_Dir,
+                                          "/FittingParameters_", i, ".RData"))
   }
 
-  ##Extract FittingData:
-  Nfiles <- list.files(Transform_CoPM_Dir)
-  Nfiles <- grep("TransformedMatrix_",Nfiles)
-  Nfiles <- length(Nfiles)
-  load(paste0(Transform_CoPM_Dir,"/TransformedMatrix_",1,".RData"))
-  temp_TCoPM <- GenePairsVsSims[which(rownames(GenePairsVsSims) %in% SigIdx),,drop =FALSE]
+  LoadingList <- vector("list",length = FittingFiles)
+  for(i in 1:length(SigIdx_list)){
+    st_g <- SigIdx_list[[i]][1]
+    ed_g <- SigIdx_list[[i]][length(SigIdx_list[[i]])]
+    st_f <- max(which(Assignment_table$istart <= st_g))
+    ed_f <- min(which(Assignment_table$iend >= ed_g))
+    LoadingList[[i]] <- st_f:ed_f
+  }
 
-  if (Nfiles >= 2) {
-    k <- 2
+  if (Nfiles == 1) {
+    load(paste0(Transform_CoPM_Dir, "/TransformedMatrix_", 1,".RData"))
+    temp_TCoPM <- GenePairsVsSims[which(rownames(GenePairsVsSims) %in% SigIdx), , drop = FALSE]
     for (j in 1:nrow(SplitMatrix)) {
-      if (j == nrow(SplitMatrix)) {
-        while(nrow(temp_TCoPM) < SplitMatrix$Length[j]){
-          load(paste0(Transform_CoPM_Dir,"/TransformedMatrix_",k,".RData"))
-          GenePairsVsSims <- GenePairsVsSims[which(rownames(GenePairsVsSims) %in% SigIdx),,drop = FALSE]
-          temp_TCoPM <- rbind.data.frame(temp_TCoPM,GenePairsVsSims)
-          k <- k + 1
-        }
-        FittingData <- temp_TCoPM
-        save(x = FittingData, file = paste0(FittingFile_Dir,"/FittingFiles_",j,".RData"))
-      }else{
-        while(nrow(temp_TCoPM) <= SplitMatrix$Length[j] & k < Nfiles){
-          load(paste0(Transform_CoPM_Dir,"/TransformedMatrix_",k,".RData"))
-          GenePairsVsSims <- GenePairsVsSims[which(rownames(GenePairsVsSims) %in% SigIdx),,drop = FALSE]
-          temp_TCoPM <- rbind.data.frame(temp_TCoPM,GenePairsVsSims)
-          k <- k + 1
-        }
-        FittingData <- temp_TCoPM[1:SplitMatrix$Length[j],,drop = FALSE]
-        temp_TCoPM <- temp_TCoPM[-c(1:SplitMatrix$Length[j]),,drop = FALSE]
-        save(x = FittingData, file = paste0(FittingFile_Dir,"/FittingFiles_",j,".RData"))
-      }
+      FittingData <- temp_TCoPM[SplitMatrix$Start[j]:SplitMatrix$End[j], , drop = FALSE]
+      save(x = FittingData, file = paste0(FittingFile_Dir, "/FittingFiles_", j, ".RData"))
     }
   }else{
-    for (j in 1:nrow(SplitMatrix)) {
-      FittingData <- temp_TCoPM[SplitMatrix$Start[j]:SplitMatrix$End[j],,drop = FALSE]
-      save(x = FittingData, file = paste0(FittingFile_Dir,"/FittingFiles_",j,".RData"))
+    PP_FittingData <- function(FittingDataIdx){
+      loading_k <- LoadingList[[FittingDataIdx]]
+      temp_TCoPM <- data.frame()
+      for (k in loading_k) {
+        load(paste0(Transform_CoPM_Dir, "/TransformedMatrix_", k, ".RData"))
+        GenePairsVsSims <- GenePairsVsSims[which(rownames(GenePairsVsSims) %in% SigIdx_list[[FittingDataIdx]]), , drop = FALSE]
+        temp_TCoPM <- rbind.data.frame(temp_TCoPM, GenePairsVsSims)
+      }
+      FittingData <- temp_TCoPM
+      save(x = FittingData, file = paste0(FittingFile_Dir, "/FittingFiles_", FittingDataIdx, ".RData"))
     }
+    mc <- getOption("mc.cores", Ncores)
+    res <- parallel::mclapply(seq(1:nrow(SplitMatrix)), PP_FittingData, mc.cores = mc)
+
   }
 
-  return(paste0("Done, Please go and check the files in '",FittingFile_Dir,"'"))
+  return(paste0("Done, Please go and check the files in '",
+                FittingFile_Dir, "'"))
 }
