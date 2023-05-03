d25c0687239c9a714136c4ff83de50069945d362
W765623192 <53879608+W765623192@users.noreply.github.com>
2023-05-03 15:22:03 -0500

    Update create_DEGMatrix.R

--- R/create_DEGMatrix.R
+++ R/create_DEGMatrix.R
@@ -6,17 +6,17 @@
 #' @examples
 #' DEGMatrix <- DEGMatrix_dat()
 create_DEGMatrix <- function(){
-  DEGMatrix <- matrix(0, nrow = 20, ncol = 10)
+  DEGMatrix <- matrix(0, nrow = 10, ncol = 20)
   set.seed(2222)
-  c <- sample(2:11,10,replace = TRUE)
+  c <- sample(2:11,20,replace = TRUE)
 
   for (i in 1:10) {
-    DEGMatrix[sample(1:20,c[i]),i] <- 1
+    DEGMatrix[i,sample(1:20,c[i])] <- 1
   }
 
 
-  rownames(DEGMatrix) <- paste0("exp_",seq(1:20))
-  colnames(DEGMatrix) <- paste0("gene_",seq(1:10))
+  rownames(DEGMatrix) <- paste0("gene_",seq(1:10))
+  colnames(DEGMatrix) <- paste0("exp_",seq(1:20))
 
   return(DEGMatrix)
 }
